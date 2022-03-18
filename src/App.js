67303d07a8ade495e18bd32faede35e8c9b52ade
Nasir <nasir-islam@hotmail.com>
2022-03-18 15:33:04 +0000

    add fetch to check back-end connection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:8080/appointments/bloodtype/A")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+  }, [])
   return (
     <h1>Hello World</h1>
   );
