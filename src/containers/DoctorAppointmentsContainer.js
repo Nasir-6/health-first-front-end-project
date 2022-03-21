df99680e82562b3bc950b4939e5626c370f8acfd
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-03-21 11:31:55 +0000

    started work on DoctorPage

---
+++ src/containers/DoctorAppointmentsContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DoctorAppointmentsContainer() {
+  return (
+    <div>DoctorAppointmentsContainer</div>
+  )
+}
--- src/pages/DoctorPage.js
+++ src/pages/DoctorPage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
 
 const DoctorPage = () => {
+  const [doctorAppointmentList, setDoctorAppointmentList] = useState([]);
+  const doctorName = "Darwin";
+  const doctorAppointmentsUrl = "http://localhost:8080/appointments/doctor/" + doctorName;
+  // useEffect(f)
+
   return (
-    <h2>DoctorPage</h2>
+    <h2>Hi Dr {doctorName}</h2>
   )
 }
 
