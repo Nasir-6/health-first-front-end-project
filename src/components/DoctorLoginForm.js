373c90c6b0df5524cb8fb1fcb9860c7dc222d026
Nasir <nasir-islam@hotmail.com>
2022-03-21 16:08:52 +0000

    add doctorForm State tracking

--- src/components/DoctorLoginForm.js
+++ src/components/DoctorLoginForm.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { useState } from 'react';
 
 const DoctorLoginForm = () => {
+
+        // Set all states to empty at start
+        const [doctorName, setDoctorName] = useState("");
+        const [doctorId, setDoctorId] = useState("");
+    
+        // create onchange event handlers!!
+        const handleDoctorNameChange = (event) => {
+            setDoctorName(event.target.value);
+        }
+    
+        const handleDoctorIdChange = (event) => {
+            setDoctorId(event.target.value);
+        }
+
+    
+        // Form submit handler
+        const handleFormSubmit = (event) => {
+            event.preventDefault();
+            //validate inputs (if empty just exits don't submit or do anything)
+            if(!doctorName || !doctorId){
+                return
+            }
+    
+            // Check if Doctor is DB!!
+    
+    
+    
+            // Lastly reset all input states - maybe not needed
+            setDoctorName("")
+            setDoctorId("")
+        }
+
+
+
+
+
   return (
-    <h2>Doctor Form</h2>
-  )
-}
+    <>
+      <h2>Doctor Form</h2>
+
+      <form>
+        <label htmlFor="doctorName">Name:</label>
+        <input
+          type="text"
+          id="doctorName"
+          name="doctorName"
+          value={doctorName}
+          onChange={handleDoctorNameChange}
+        />
+
+        <label htmlFor="doctorId">Id:</label>
+        <input
+          type="number"
+          id="doctorId"
+          name="doctorId"
+          value={doctorId}
+          onChange={handleDoctorIdChange}
+        />
+
+        <input type="submit" value="Login" />
+      </form>
+    </>
+  );
+};
 
 export default DoctorLoginForm;
