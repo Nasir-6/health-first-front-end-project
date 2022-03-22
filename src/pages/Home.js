c47240c3be9fd832f5898572174ce715d0c0728b
Nasir <nasir-islam@hotmail.com>
2022-03-22 12:31:47 +0000

    complete landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import doctorImage from '../images/doctorImage.jpg'
 
 const Home = () => {
   return (
     <>
-    <h2>We put your health first</h2>
-
-      <body>
-      <img src = {doctorImage} className='doctorImage'></img>
-      </body>
-
-      </>
-    
-  )
-}
+      <div className="slogan-container">
+        <div className="slogan-overlay">
+          <h2 className="slogan">We put your health first</h2>
+        </div>
+      </div>
+      <div className="about-us">
+          <h2 className='about-us-title'>About us</h2>
+          <p className='about-us-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore natus consectetur modi aspernatur! Nesciunt, quia numquam. Consectetur deleniti vero provident voluptates, aspernatur in reiciendis voluptate corporis optio possimus, ipsam delectus.</p>
+      </div>
+    </>
+  );
+};
 
 export default Home;
