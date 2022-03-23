44090895363c7871ed5de9994420ea64bec4689d
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-03-23 04:04:18 +0000

    test for presence of slogan

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('is slogan present', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/We put your health first/i);
   expect(linkElement).toBeInTheDocument();
 });
