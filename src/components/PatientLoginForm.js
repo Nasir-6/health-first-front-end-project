439f18bd447d8c7c293eaf819c2dd4f408e46444
Nasir <nasir-islam@hotmail.com>
2022-03-24 15:04:17 +0000

    add patient login functionality

--- src/components/PatientLoginForm.js
+++ src/components/PatientLoginForm.js
@@ -1,8 +1,114 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
-const PatientLoginForm = () => {
+const PatientLoginForm = ({setIsLoggedIn, updateCurrentUser}) => {
+
+
+          // Set all states to empty at start
+          const [patientName, setpatientName] = useState("");
+          const [patientNhsId, setpatientNhsId] = useState("");
+          const [invalidTextWarning, setInvalidTextWarning] = useState("");
+          const navigate = useNavigate();
+      
+          // // create onchange event handlers!!
+          const handlepatientNameChange = (event) => {
+              setpatientName(event.target.value);
+          }
+      
+          const handlepatientNhsIdChange = (event) => {
+              setpatientNhsId(event.target.value);
+          }
+  
+  
+          // // Fetch request to grab patientByID
+          const getpatientById = async (id) =>{
+              const patient = await fetch(`http://localhost:8080/patients/${id}`)
+              .then(response => response.json())
+              // .then(data => console.log(data))
+              .catch(error => console.error(error))
+  
+              return patient;            
+          }
+      
+          // Form submit handler
+          // MAKE SURE TO HAVE async and await to ensure promise is returned before moving onto next line of code!!!
+          const handleFormSubmit = async (event) => {
+              event.preventDefault();
+              //validate inputs (if empty just exits don't submit or do anything)
+
+              
+              
+              setInvalidTextWarning(" ");
+  
+              if(!patientName || !patientNhsId){
+                  setInvalidTextWarning("Please enter missing fields");
+                  return
+              }
+              
+      
+              // Check if patient is DB!!
+              const patientInDb = await getpatientById(patientNhsId);
+                    
+              
+              if(patientInDb === null || patientInDb === undefined || patientInDb.status === 404){
+                console.log('Im here');
+                  setInvalidTextWarning("Invalid name or ID");
+                  return
+              } else if(patientInDb.patientName.toLowerCase() != patientName.toLowerCase()){
+                  setInvalidTextWarning("Incorrect name or ID");
+                  return
+              }else if(patientInDb.patientName.toLowerCase() === patientName.toLowerCase()){
+                  setInvalidTextWarning(" ");
+                  setIsLoggedIn(true)
+                  // Also set the currentUser
+                  updateCurrentUser({
+                      name: patientName,
+                      id: patientNhsId
+                  })
+                  navigate(`/patient/${patientInDb.patientNhsId}`);
+                  return
+              } else {
+                  setInvalidTextWarning("Something went wrong");
+              }
+  
+          }
+  
+  
+
+
   return (
+    <>
     <h2>Patient Form</h2>
+    <form onSubmit={handleFormSubmit} className="login-form">
+      <div className="name-input-container">
+      <label htmlFor="patientName">Name</label>
+      <input
+        type="text"
+        id="patientName"
+        name="patientName"
+        value={patientName}
+        onChange={handlepatientNameChange}
+      />
+      </div>
+
+      <div className="id-input-container">
+      <label htmlFor="patientNhsId" className="id-label">ID</label>
+      <input
+        type="number"
+        id="patientNhsId"
+        name="patientNhsId"
+        value={patientNhsId}
+        onChange={handlepatientNhsIdChange}
+      />
+      </div>
+      
+
+      <p className="invalidText">{invalidTextWarning}</p>
+
+      <input type="submit" value="Login" className="button" />
+    </form>
+  </>
   )
 }
 
