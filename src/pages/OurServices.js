d0dbb19a90dfe084896833c46ab43173bb024bec
Connie Bernardin <conniebernardin@googlemail.com>
2022-03-24 17:27:58 +0000

    formatting our services page containers

--- src/pages/OurServices.js
+++ src/pages/OurServices.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const OurServices = () => {
   return (
-    <h2>Our Services</h2>
+    <div className="our-services-page">
+
+    <h1 className='our-services-header'>Health First</h1>
+    <h3 className="subheader">Services we offer</h3>
+
+    <div className="our-services-container">
+
+        <div className="our-services-left">
+            <h3 className="who-are-we">Who are we?</h3>
+
+        </div>
+
+        <div className="our-services-center">
+            <h3 className="who-are-we">Who are we?</h3>
+
+        </div>
+
+        <div className="our-services-right">
+            <h4 className='slogan-tag'>Putting your health first since 2022</h4>
+        </div>
+
+    </div>
+</div>
+
   )
 }
 
