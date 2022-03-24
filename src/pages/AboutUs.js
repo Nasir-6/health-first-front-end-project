aa50141572d49c70fee34725aa84ac301074f1f8
Connie Bernardin <conniebernardin@googlemail.com>
2022-03-24 16:12:03 +0000

    formatting containers on about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 const AboutUsPage = () => {
   return (
-   
-   <div className="about-us-container">
-    <h1 className='about-us-header'>Health First</h1>
-    </div>
+      <div className="about-us-page">
+
+          <h1 className='about-us-header'>Health First</h1>
+          <h3 className="subheader">About us</h3>
+
+          <div className="about-us-container">
+
+              <div className="about-us-left">
+                  <h3 className="who-are-we">Who are we?</h3>
+              </div>
+
+              <div className="about-us-right">
+                  <h3 className="images">images</h3>
+              </div>
+
+          </div>
+      </div>
+ 
   )
 }
 
