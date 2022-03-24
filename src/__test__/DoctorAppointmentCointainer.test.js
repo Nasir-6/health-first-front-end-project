17e3adf5d1bf5abcc1abdcca9a8bd1029cc0be21
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-03-24 16:32:48 +0000

    added test for deleting appointment from mock list

--- src/__test__/DoctorAppointmentCointainer.test.js
+++ src/__test__/DoctorAppointmentCointainer.test.js
@@ -1,7 +1,14 @@
 import { render, screen, fireEvent} from '@testing-library/react';
 import DoctorAppointmentsContainer from '../containers/DoctorAppointmentsContainer';
 
-const doctorAppointmentsList = [];
+let doctorAppointmentsList = [];
+const mockDeleteAppointment = async (e) => {
+    const appointmentId = e.currentTarget.id
+
+    doctorAppointmentsList = doctorAppointmentsList
+        .filter(appointment => appointment.appointmentId !== appointmentId);
+
+  }
 
 describe("Doctor appointment container tests", () => {
     beforeAll(() => {
@@ -15,7 +22,7 @@ describe("Doctor appointment container tests", () => {
         })
         doctorAppointmentsList.push({
             appointmentDate: "1997-10-09",
-            appointmentId: 6,
+            appointmentId: 2,
             appointmentTime: "11:23:00",
             doctorName: "Dr Darwin",
             patientName: "Nasir",
@@ -38,4 +45,20 @@ describe("Doctor appointment container tests", () => {
         // Then
         expect(numAppointments).toBe(2);
   })
+  test('can delete appointment', () => {
+    // Given
+    window.history.pushState({}, "", "doctor/1");
+    render(
+        <DoctorAppointmentsContainer 
+        doctorAppointmentsList={doctorAppointmentsList}
+        handleDeleteAppointment={() => {}}
+        updateAppointment={() => {}}
+        />
+    )
+    const appointmentElements = screen.getAllByText(/Patient:/i);
+    // When
+    const numAppointments = appointmentElements.length;
+    // Then
+    expect(numAppointments).toBe(2);
+})
 })
