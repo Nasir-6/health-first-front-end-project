571930dbb2f135c6dbe430713bcbdd01a8b22807
Connie Bernardin <conniebernardin@googlemail.com>
2022-03-24 16:36:41 +0000

    adding text to about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import DoctorHolding from '../images/DoctorHolding.jpg';
+import LaptopImage from '../images/LaptopImage.jpg';
 
 const AboutUsPage = () => {
   return (
@@ -11,10 +13,13 @@ const AboutUsPage = () => {
 
               <div className="about-us-left">
                   <h3 className="who-are-we">Who are we?</h3>
+
+                  <p className='about-info'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore natus consectetur modi aspernatur! Nesciunt, quia numquam. Consectetur deleniti vero provident voluptates, aspernatur in reiciendis voluptate corporis optio possimus, ipsam delectus.</p>
               </div>
 
               <div className="about-us-right">
-                  <h3 className="images">images</h3>
+              <img src={LaptopImage} className='laptop-image'/>
+              <img src={DoctorHolding} className='doctor-image'/>
               </div>
 
           </div>
