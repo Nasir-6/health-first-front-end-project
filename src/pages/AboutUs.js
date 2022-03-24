def04707c641e72169e816cc3ac5126a737051a6
Connie Bernardin <conniebernardin@googlemail.com>
2022-03-24 15:35:29 +0000

    about us title page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AboutUsPage = () => {
   return (
-    <h2>Health First</h2>
+   
+   <div className="about-us-container">
+    <h1 className='about-us-header'>Health First</h1>
+    </div>
   )
 }
 
